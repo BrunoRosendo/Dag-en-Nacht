f68439a2bbb1777081eeeb40662096d20d968f1b
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-01-06 21:16:35 +0000

    GameOver predicate

--- src/logic.pl
+++ src/logic.pl
@@ -1,7 +1,7 @@
 :- consult('board.pl').
 
 /**
- * validatePlaceStone(+GameState, +Move)
+ * validatePlaceStone(+GameState, +Pos)
  */
 validatePlaceStone((Board, _), (X, Y)) :-
     getCell(Board, X, Y, Cell),
@@ -9,14 +9,14 @@ validatePlaceStone((Board, _), (X, Y)) :-
     state(Cell, e). % Can only place a stone on an empty square
 
 /**
- * placeStone(+GameState, +Move, -GameState)
+ * placeStone(+GameState, +Pos, -NewGameState)
  */
 placeStone((Board, Player), (X, Y), (NewBoard, NextPlayer)) :-
     replaceCell(Board, X, Y, b-Player, NewBoard),
     switchColor(Player, NextPlayer).
 
 /**
- * validateShiftStone(+GameState, +Move)
+ * validateShiftStone(+GameState, +Pos, +NewPos)
  */
 validateShiftStone((Board, Player), (X, Y), (NewX, NewY)) :-
     getCell(Board, X, Y, Cell),
@@ -30,7 +30,7 @@ validateShiftStone((Board, Player), (X, Y), (NewX, NewY)) :-
     state(NewCell, e). % Can only shift to an empty cell
 
 /**
- * shiftStone(+GameState, +Move, +Pos, -NewGameState)
+ * shiftStone(+GameState, +Pos, +NewPos, -NewGameState)
  */
 shiftStone((Board, Player), (X, Y), (NewX, NewY), (NewBoard, NextPlayer)) :-
     replaceCell(Board, X, Y, b-e, Board1),
@@ -40,14 +40,47 @@ shiftStone((Board, Player), (X, Y), (NewX, NewY), (NewBoard, NextPlayer)) :-
 /**
  * move(+GameState, +Move, -NewGameState)
  */
-move(GameState, Move, NewGameState) :-
+move(GameState, place-Pos, NewGameState) :-
     validatePlaceStone(GameState, Move),
-    placeStone(GameState, Move, NewGameState).
+    placeStone(GameState, Pos, NewGameState).
 
-move(GameState, (X, Y), NewGameState) :-
+move(GameState, shift-(X, Y), NewGameState) :-
     X1 is X - 1,
     validateShiftStone(GameState, (X, Y), (X1, Y)),
     % The position must be given by input. This is just a placeholder
     shiftStone(GameState, (X, Y), (X1, Y), NewGameState).
 
-gameOver(GameState, )
+/**
+ * checkWin(+Vectors, +Player, +NumPieces)
+ * Check if the player won, by analyzing the list of vectors
+ * A vector is either a row, line or diagonal
+ */
+checkWin(Vectors, Player, NumPieces) :- 
+    getWinCondition(Player, NumPieces, WinCondition),
+    checkWin(Vectors, WinCondition).
+
+checkWin([Vector | T], WinCondition) :-
+    sublist(Vector, WinCondition, _), ! ;
+    checkWin(T, WinCondition).
+
+
+/**
+ * getWinCondition(+Player, +NumPieces, -WinCondition)
+ * Generates a win condition (list of player-owned cells) with the given number of pieces
+ */
+getWinCondition(Player, NumPieces, WinCondition) :- getWinCondition(Player, NumPieces, WinCondition, []).
+getWinCondition(_, 0, WinCondition, WinCondition) :- !.
+getWinCondition(Player, NumPieces, WinCondition, Acc) :-
+    append(Acc, [_-Player], Acc1),
+    NumPieces1 is NumPieces - 1,
+    getWinCondition(Player, NumPieces1, WinCondition, Acc1).
+
+/**
+ * gameOver(+GameState, -Winner)
+ */
+gameOver((Board, Player), Player) :-
+    transpose(Board, Cols),
+    whiteDiagonals(Board, WhiteDiags),
+    (checkWin(Board, Player, 5); % Rows
+    checkWin(Cols, Player, 5);
+    checkWin(WhiteDiags, Player, 4)).
