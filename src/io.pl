a59097c5a338f150073337bf09d2422a9dac9cdb
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-01-14 16:53:34 +0000

    Validating number input

--- src/io.pl
+++ src/io.pl
@@ -1,8 +1,25 @@
 :-use_module(library(between)).
+:- consult('utils.pl').
 
 % Clears the terminal screen
 clear :- write('\e[2J').
 
+/**
+ * invalidDigit(+C)
+ *
+ * Checks if the char is an invalid digit and clears input if so
+ */
+invalidDigit(C) :-
+    (C < 48 ; C > 57),
+    skip_line.
+
+validDigit(C) :- not(invalidDigit(C)), !.
+
+/**
+ * readNumber(-X)
+ *
+ * Reads a number from input
+ */
 readNumber(X) :- readNumber(X, 0).
 
 readNumber(X, X) :-
@@ -11,14 +28,22 @@ readNumber(X, X) :-
 
 readNumber(X, Acc) :-
     get_code(C),
-    C >= 48,
-    C =< 57, !,
+    validDigit(C),
     Real is C - 48,
     Tmp is Acc * 10,
     Acc1 is Tmp + Real,
     readNumber(X, Acc1).
 
+/**
+ * readUntilBetween(+Min, +Max, -Value)
+ *
+ * Reads a number from input until the user inserts one between two values
+ */
 readUntilBetween(Min, Max, Value) :-
-    repeat, % until we find the solution
+    format('| Chose an option [~d-~d]: ', [Min, Max]),
     readNumber(Value),
     between(Min, Max, Value), !.
+
+readUntilBetween(Min, Max, Value) :-
+    format('| Invalid option! Please choose between ~d and ~d~n', [Min, Max]),
+    readUntilBetween(Min, Max, Value).
